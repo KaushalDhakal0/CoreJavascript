ba79c6359745c4610372c9c0b76e8f4466750d68
Kaushal <kaushaldhakal90@gmail.com>
2022-12-14 23:52:07 +0545

    polyfill for Array.sort()

--- problem5.js
+++ problem5.js
@@ -5,12 +5,33 @@
 
 // write a poolyfill for Array.sort();
 
-const customSort = arr =>{
-if(arr.length < 2) return arr;
+// const customSort = arr =>{
+// if(arr.length < 2) return arr;
+//     let sortedArray = [];
+//     for (let i = 0; i < arr.length; i++) {
+//         let j = 0;
+//         const currentNumber = arr[i];
+//         while(sortedArray[j] < currentNumber){
+//             j++;
+//         }
+//         sortedArray.splice(j,0,currentNumber);
+//     }
+//     return sortedArray;
+// }
+
+
+
+// console.log(customSort([4,5,6,2,3,1]));
+// let cc = [1,2,3,4];
+// cc.splice(0,cc.length,99);
+// console.log(cc);
+
+Array.prototype.customSort = function(){
+if(this.length < 2) return this;
     let sortedArray = [];
-    for (let i = 0; i < arr.length; i++) {
+    for (let i = 0; i < this.length; i++) {
         let j = 0;
-        const currentNumber = arr[i];
+        const currentNumber = this[i];
         while(sortedArray[j] < currentNumber){
             j++;
         }
@@ -19,9 +40,5 @@ if(arr.length < 2) return arr;
     return sortedArray;
 }
 
-
-
-// console.log(customSort([4,5,6,2,3,1]));
-let cc = [1,2,3,4];
-cc.splice(0,cc.length,99);
-console.log(cc);
+const sorted = [4,5,6,2,3,1,0,-87].customSort();
+console.log("=====",sorted);
