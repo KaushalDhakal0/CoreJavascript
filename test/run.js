9c977c7c65785fee8e7a0bc6a50f98ce0bf48658
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-06-13 21:11:18 +0545

    Async Js visualized

--- test/run.js
+++ test/run.js
@@ -10,8 +10,9 @@ function scheduleWork() {
   };
   const btn = document.getElementById('btn');
   btn.addEventListener('click', scheduleWork); 
+  //Order when button is clicked through page.by clicking on button ====> m3, m4, m5, m1, m2, t1, t2, m6, t3
   //If we click button programmatically it will act Synchronously as schedule work will go to callStack directly instead of Task Queue.
-  btn.click(); 
+  // btn.click(); //===>m1,m2,m3,m4,m5,t1,t2,m6,t3
   Promise.resolve().then(() => console.log('m3'));
   Promise.resolve().then(() => console.log('m4'));
   Promise.resolve().then(() => console.log('m5'));
