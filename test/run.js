28cecd1c0ea52195f5d8ce2889d96e53a8885a89
KaushalDhakal0 <kaushaldhakal90@gmail.com>
2023-06-13 21:01:08 +0545

    programmatic Click

--- test/run.js
+++ test/run.js
@@ -7,10 +7,12 @@ function scheduleWork() {
       Promise.resolve().then(() => console.log('m6'));
       setTimeout(() => console.log('t3'), 0);
     }, 0);
-  }
-  
+  };
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', scheduleWork); 
+  //If we click button programmatically it will act Synchronously as schedule work will go to callStack directly instead of Task Queue.
+  btn.click(); 
   Promise.resolve().then(() => console.log('m3'));
   Promise.resolve().then(() => console.log('m4'));
   Promise.resolve().then(() => console.log('m5'));
-  const btn = document.getElementById('btn');
-  btn.addEventListener('click', scheduleWork);  
+  
